b607f03eba34b4bff5e042ddec101992efbb6e46
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-10-12 14:49:14 +0200

    CocoaPods install

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '12.0'
+use_frameworks!
+
+target 'Espeo' do
+
+   pod 'ReachabilitySwift'
+   pod 'Alamofire'
+
+end
---
+++ Pods/Target Support Files/Pods-Espeo/Pods-Espeo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Espeo : NSObject
+@end
+@implementation PodsDummy_Pods_Espeo
+@end
